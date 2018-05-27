2f69d8d903bf32aaa87c9fd4bf0badda2a8c5678
Luca Maraschi <luca.maraschi@gmail.com>
2018-05-26 19:32:22 -0700

    Added aws linking

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 SRC_PATH=~/src/lm
 mkdir -p ~/.ssh
+mkdir -p ~/.aws
 mkdir -p $SRC_PATH
 
 git clone https://github.com/lucamaraschi/secrets.git $SRC_PATH/secrets
@@ -11,6 +12,8 @@ git clone https://github.com/lucamaraschi/dotfiles $SRC_PATH/dotfiles
 cp $SRC_PATH/secrets/id_rsa* ~/.ssh/
 sudo chmod 600 ~/.ssh/id_rsa
 sudo chmod 600 ~/.ssh/id_rsa.pub
+
+ln -s $SRC_PATH/secrets/aws ~/.aws
 
 cd $SRC_PATH/dotfiles && pwd $SRC_PATH/dotfiles
 source $SRC_PATH/dotfiles/install.sh
