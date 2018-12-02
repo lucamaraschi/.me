ae6dfa5d7de21662375d96ff4bdf5b90a4b01c75
Luca Maraschi <luca.maraschi@gmail.com>
2018-12-02 14:32:09 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,24 @@ mkdir -p ~/.ssh
 mkdir -p ~/.aws
 mkdir -p $SRC_PATH
 
+echo "Checking Xcode CLI tools"
+# Only run if the tools are not installed yet
+# To check that try to print the SDK path
+xcode-select -p &> /dev/null
+if [ $? -ne 0 ]; then
+  echo "Xcode CLI tools not found. Installing them..."
+  touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+  PROD=$(softwareupdate -l |
+    grep "\*.*Command Line" |
+    head -n 1 | awk -F"*" '{print $2}' |
+    sed -e 's/^ *//' |
+    tr -d '\n')
+  softwareupdate -i "$PROD" -v;
+else
+  sudo xcodebuild -license accept
+  echo "Xcode CLI tools OK"
+fi
+
 git clone https://github.com/lucamaraschi/secrets.git $SRC_PATH/secrets
 
 git clone https://github.com/lucamaraschi/dotfiles $SRC_PATH/dotfiles
