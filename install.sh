39307e7c409c0e1b122e61fc50302350a8b65333
Luca Maraschi <luca.maraschi@gmail.com>
2018-12-09 15:17:08 -0800

    Update install

--- install.sh
+++ install.sh
@@ -18,19 +18,21 @@ if [ $? -ne 0 ]; then
     sed -e 's/^ *//' |
     tr -d '\n')
   softwareupdate -i "$PROD" -v;
-else
-  sudo xcodebuild -license accept
-  echo "Xcode CLI tools OK"
 fi
+
+sudo xcodebuild -license accept
+echo "Xcode CLI tools OK"
 
 git clone https://github.com/lucamaraschi/secrets.git $SRC_PATH/secrets
 
 git clone https://github.com/lucamaraschi/dotfiles $SRC_PATH/dotfiles
 
+mkdir -p ~/.ssh
 cp $SRC_PATH/secrets/id_rsa* ~/.ssh/
 sudo chmod 600 ~/.ssh/id_rsa
 sudo chmod 600 ~/.ssh/id_rsa.pub
 
+mkdir -p ~/.aws
 ln -s $SRC_PATH/secrets/aws ~/.aws
 
 cd $SRC_PATH/dotfiles && pwd $SRC_PATH/dotfiles
