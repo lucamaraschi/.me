211bf75154a290065ce46459dddf4e4874a31029
Luca Maraschi <luca.maraschi@gmail.com>
2018-12-13 14:09:51 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,24 +5,10 @@ mkdir -p ~/.ssh
 mkdir -p ~/.aws
 mkdir -p $SRC_PATH
 
-if [ ! "$(which git)" ]; then
-        XCODE_MESSAGE="$(osascript -e 'tell app "System Events" to display dialog "Please click install when Command Line Developer Tools appears"')"
-        if [ "$XCODE_MESSAGE" = "button returned:OK" ]; then
-            xcode-select --install
-        else
-            echo "You have cancelled the installation, please rerun the installer."
-            # you have forgotten to exit here
-            exit
-        fi
+if test ! $(which brew); then
+	echo "Installing homebrew"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
-
-until [ "$(which git)" ]; do
-        echo -n "."
-        sleep 1
-done
-echo ""
-
-echo 'Xcode has finished installing'
 
 sudo xcodebuild -license accept
 echo "Xcode CLI tools OK"
