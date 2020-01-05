9657c97ef610f4f817d12495ba1751b5f5dc65ae
Luca Maraschi <luca.maraschi@gmail.com>
2020-01-05 10:05:21 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,8 +17,10 @@ fi
 sudo xcodebuild -license accept
 echo "Xcode CLI tools OK"
 
+[ -d "$SRC_PATH/secrets" ] && echo "Directory $SRC_PATH/secrets exists. Deleteling existing one" && rm -rf $SRC_PATH/secrets
 git clone https://github.com/lucamaraschi/secrets.git $SRC_PATH/secrets
 
+[ -d "$SRC_PATH/dotfiles" ] && echo "Directory $SRC_PATH/dotfiles exists. Deleteling existing one" && rm -rf $SRC_PATH/dotfiles
 git clone --branch 2.0 https://github.com/lucamaraschi/dotfiles $SRC_PATH/dotfiles
 
 mkdir -p ~/.ssh
